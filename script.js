e8608adf515088612b92c08dd4b40d0371bdb433
George Leslie <georgeleslie06@gmail.com>
2024-10-12 06:55:06 +0700

    updat6 Nav!

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    this.classList.toggle('open');
-    document.querySelector('.nav-menu').classList.toggle('active');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let dropdowns = document.querySelectorAll('.dropdown');
+
+// Toggle hamburger menu
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+// Handle dropdowns for mobile
+dropdowns.forEach(dropdown => {
+    dropdown.onclick = (e) => {
+        // Prevent click from closing other dropdowns
+        e.stopPropagation();
+        
+        dropdown.classList.toggle('open');
+    };
 });
